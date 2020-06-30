cee715535be30fcc8a8a71fefcf2afdff1f14d2f
sorrelbri <sorrel.bri.june@gmail.com>
2020-06-30 14:32:32 -0700

    config db connection and import in app.js, add db setup info to readme

--- data/knexfile.js
+++ data/knexfile.js
@@ -1,13 +1,13 @@
-// require('dotenv').config();
+require("dotenv").config();
 
-const default = {
-  client: 'postgresql',
-  seeds: { directory: './data/seeds' },
-  migrations: { directory: './data/migrations'}
-},
+const sharedConfig = {
+  client: "postgresql",
+  seeds: { directory: "./data/seeds" },
+  migrations: { directory: "./data/migrations" },
+};
 
 module.exports = {
-  development: { ...default, connection: '' },
-  test: { ...default, connection: '' },
-  production: {...default, connection: ''}
-}
+  development: { ...sharedConfig, connection: "" },
+  test: { ...sharedConfig, connection: "" },
+  production: { ...sharedConfig, connection: "" },
+};
